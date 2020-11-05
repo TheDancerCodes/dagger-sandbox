45edeaad9767e068690047d86a21d0fdb98b0fe1
Roger Taracha <rojtaracha@gmail.com>
2020-11-05 15:06:19 +0300

    Injecting Activities with Dagger2 and @ContributesAndroidInjector: (#2)
    * Create ActivityBuildersModule - Contains the potential clients that you can inject into.
    * NOTE: If you are using @ContributesAndroidInjector annotation, you need to extend your class using DaggerAppCompatActivity. See AuthActivity.
    * See the simple example of string injection into the AuthActivity.

---
+++ app/src/main/java/com/thedancercodes/daggersandbox/di/ActivityBuildersModule.java
@@ -0,0 +1,23 @@
+package com.thedancercodes.daggersandbox.di;
+
+import com.thedancercodes.daggersandbox.AuthActivity;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
+
+/**
+ * Contains the potential clients that you can inject into.
+ */
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+
+    @Provides
+    static String someString(){
+        return "this is a test string";
+    }
+
+}
