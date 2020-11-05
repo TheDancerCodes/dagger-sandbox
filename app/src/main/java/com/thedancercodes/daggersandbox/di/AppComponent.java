f7a6505a722888760b23c55ccd74430fe3919328
Taracha <rojtaracha@gmail.com>
2020-11-05 14:04:52 +0300

    Dagger2 Application Component: * Build a dagger app level component class. * Reference BaseApplication in manifest file. * Bind BaseApplication class to AppComponent.

---
+++ app/src/main/java/com/thedancercodes/daggersandbox/di/AppComponent.java
@@ -0,0 +1,46 @@
+package com.thedancercodes.daggersandbox.di;
+
+import android.app.Application;
+
+import com.thedancercodes.daggersandbox.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+/**
+ * Component interface that persist the entire lifetime of the application.
+ *
+ * It creates a graph of the dependencies in your project that it can use to find out where it
+ * should get those dependencies when they are needed.
+ *
+ * Here we inject BaseApplication into AppComponent & BaseApplication will be a client of the
+ * AppComponent service.
+ */
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    /*
+     * Overriding the component builder method.
+     *
+     * Basically, we are overriding the regular builder and returning a type of AppComponent.
+     *
+     * @BindsInstance used when you want to bind a particular instance of an object to the component
+     * at the time of its construction.
+     */
+    @Component.Builder
+    interface Builder {
+
+        // Binding an Application instance to the ApplicationComponent
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+
+}
