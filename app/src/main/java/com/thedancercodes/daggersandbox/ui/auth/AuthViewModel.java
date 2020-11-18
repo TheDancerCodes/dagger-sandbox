ed854e3f17f99f64fea38fe5ecdc7acb7fadb102
Taracha <rojtaracha@gmail.com>
2020-11-18 16:31:58 +0300

    LiveData Reactive Streams and Flowables (MVVM, Retrofit, and RxJava): * Convert a reactive Flowable object to LiveData so that it can be fed back to the UI to be consistent with our MVVM architecture.
    * app/build.gradle - Add Reactive Streams dependency to convert the Flowable object to LiveData object.
    
    * AuthViewModel
      * Create a MediatorLiveData object
      * Initiate call to the API.
      * Convert the Flowable to the LiveData object using LiveDataReactiveStreams dependency.
    
    * AuthActivity
      * Set up everything for executing the request and getting the data into the UI.
      * EditText widget to capture the user id.
      * Attach OnClickListener to Login Button.
      * Create an attemptLogin() function.
      * Subscribing to the observers.
        * We want to observe the MediatorLiveData object (authUser) in the ViewModel from the UI.
        * Any changes made to the authUser LiveData object will be updated to the UI.
        * Create a new method - subscribeObservers()
    
    * MVVM Recap:
      * The AuthActivity starts and the observers will subscribe [ subscribeObservers() ] and begin observing the LiveData in the ViewModel. [ observeUser() ]
    
      * When the button is clicked it calls the authenticateWithId() method. If the user is authenticated successfully, the value of that user will get set to the authUser MediatorLiveData object.
    
      * The observers will be updated immediately and onChanged() method in AuthActivity under subscribeObservers() will be triggered and print out the user.

--- app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthViewModel.java
@@ -2,6 +2,10 @@ package com.thedancercodes.daggersandbox.ui.auth;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.thedancercodes.daggersandbox.models.User;
@@ -9,9 +13,6 @@ import com.thedancercodes.daggersandbox.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
-import io.reactivex.Observer;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
@@ -20,35 +21,37 @@ public class AuthViewModel extends ViewModel {
 
     private final AuthApi authApi;
 
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+
     @Inject
     public AuthViewModel(AuthApi authApi) {
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: ViewModel is working...");
-
-        // Make request and run it on a background thread
-        authApi.getUser(1)
-                .toObservable() // Convert Flowable object to an Observable
-                .subscribeOn(Schedulers.io()) // Subscribe on a background thread
-                .subscribe(new Observer<User>() { // New observer to observe the Observable
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(@NonNull User user) {
-                        Log.d(TAG, "onNext: " + user.getEmail());
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Log.e(TAG, "onError: ", e);
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
     }
+
+    // Initiate call to the API. The source is the API call from the Flowable object.
+    // Here we are converting the Flowable to the LiveData object.
+    public void authenticateWithId(int userId) {
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                authApi.getUser(userId)
+                        .subscribeOn(Schedulers.io()));
+
+        // Setting source (above) to authUser (below) using MediatorLiveData
+        authUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                // Update any observes using setValue()
+                authUser.setValue(user);
+
+                // Remove source since we no longer need to listen to it.
+                authUser.removeSource(source);
+            }
+        });
+    }
+
+    // LiveData object of required data type (User) that we are returning to the UI
+    public LiveData<User> observeUser() {
+        return authUser;
+    }
+
 }
