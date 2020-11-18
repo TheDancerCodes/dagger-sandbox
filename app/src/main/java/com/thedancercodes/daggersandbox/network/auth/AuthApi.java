ca1c63c71250679a7ff1b25fe53f2247813feb44
Taracha <rojtaracha@gmail.com>
2020-11-18 15:23:28 +0300

    Retrofit Calls and RxJava: * Get Rx dependencies required to return a Flowable object from the Retrofit request.
    * Inside ViewModel we can convert the Flowable object into a LiveData object.
    
    * Add RxJava dependency.
    
    * Add RxJava Retrofit Adapter to convert retrofit Call objects to Flowable objects.
      * https://github.com/square/retrofit/tree/master/retrofit-adapters/rxjava2
    
    * AppModule - apply the rx retrofit adapter to the retrofit instance.
    
    * Model the response to return the data we require.
      * Build the real model for the response.
      * models/User.java
    
    * AuthApi - return Flowable object from the Retrofit call. Set it up to receive a parameter of user id.
    
    * Add internet permissions in AndroidManifest.xml
    
    * AuthViewModel - Perform a test request to ensure everything is working.
      * Since API is already injected, we just need to make the request.

--- app/src/main/java/com/thedancercodes/daggersandbox/network/auth/AuthApi.java
+++ app/src/main/java/com/thedancercodes/daggersandbox/network/auth/AuthApi.java
@@ -1,15 +1,17 @@
 package com.thedancercodes.daggersandbox.network.auth;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
+import com.thedancercodes.daggersandbox.models.User;
+
+import io.reactivex.Flowable;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Interface that holds the methods for accessing the API endpoints required for Authentication.
  */
 public interface AuthApi {
 
-    @GET
-    Call<ResponseBody> getFakeStuff();
+    @GET("users/{id}")
+    Flowable<User> getUser(@Path("id") int id);
 
 }
--- app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthViewModel.java
@@ -4,9 +4,15 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.thedancercodes.daggersandbox.models.User;
 import com.thedancercodes.daggersandbox.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -19,11 +25,30 @@ public class AuthViewModel extends ViewModel {
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: ViewModel is working...");
 
-        // Check that the AuthApi is being injected as a dependency.
-        if (this.authApi == null) {
-            Log.d(TAG, "AuthViewModel: auth api is NULL.");
-        } else {
-            Log.d(TAG, "AuthViewModel: auth api is NOT NULL.");
-        }
+        // Make request and run it on a background thread
+        authApi.getUser(1)
+                .toObservable() // Convert Flowable object to an Observable
+                .subscribeOn(Schedulers.io()) // Subscribe on a background thread
+                .subscribe(new Observer<User>() { // New observer to observe the Observable
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull User user) {
+                        Log.d(TAG, "onNext: " + user.getEmail());
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.e(TAG, "onError: ", e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
