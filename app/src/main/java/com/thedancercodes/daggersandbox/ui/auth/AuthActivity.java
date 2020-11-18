9810c3ec1452b12427e996688c95bb40c1801fd8
Roger Taracha <rojtaracha@gmail.com>
2020-11-18 16:34:54 +0300

    LiveData Reactive Streams and Flowables (MVVM, Retrofit, and RxJava): (#9)
    * Convert a reactive Flowable object to LiveData so that it can be fed back to the UI to be consistent with our MVVM architecture.
    
    * `app/build.gradle` - Add Reactive Streams dependency to convert the Flowable object to LiveData object.
    
    * **AuthViewModel**
      * Create a `MediatorLiveData` object
      * Initiate call to the API.
      * Convert the Flowable to the LiveData object using `LiveDataReactiveStreams` dependency.
    
    * **AuthActivity**
      * Set up everything for executing the request and getting the data into the UI.
      * **EditText** widget to capture the user id.
      * Attach OnClickListener to Login Button.
      * Create an `attemptLogin()` function.
      * Subscribing to the observers.
        * We want to observe the `MediatorLiveData` object (authUser) in the ViewModel from the UI.
        * Any changes made to the `authUser` LiveData object will be updated to the UI.
        * Create a new method - `subscribeObservers()`
    
    * MVVM Recap:
      * The **AuthActivity** starts and the observers will subscribe [ subscribeObservers() ] and begin observing the LiveData in the ViewModel. [ observeUser() ]
    
      * When the button is clicked it calls the `authenticateWithId()` method. If the user is authenticated successfully, the value of that user will get set to the authUser MediatorLiveData object.
    
      * The observers will be updated immediately and `onChanged()` method in AuthActivity under subscribeObservers() will be triggered and print out the user.

--- app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthActivity.java
+++ app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthActivity.java
@@ -1,26 +1,32 @@
 package com.thedancercodes.daggersandbox.ui.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.bumptech.glide.RequestManager;
 import com.thedancercodes.daggersandbox.R;
+import com.thedancercodes.daggersandbox.models.User;
 import com.thedancercodes.daggersandbox.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AuthActivity";
 
     private AuthViewModel viewModel;
+
+    private EditText userId;
 
     // Used to instantiate the ViewModel
     @Inject
@@ -37,10 +43,34 @@ public class AuthActivity extends DaggerAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+        // Instantiate EditText
+        userId = findViewById(R.id.user_id_input);
+
+        // Attach OnClickListener to the Login button
+        findViewById(R.id.login_button).setOnClickListener(this);
+
         // Instantiate the ViewModel
         viewModel = ViewModelProviders.of(this, providerFactory).get(AuthViewModel.class);
 
         setLogo();
+
+        subscribeObservers();
+    }
+
+    // This method helps us observe the MediatorLiveData object (authUser) in AuthViewModel
+    // from the UI.
+    // Any changes made to the authUser LiveData object will be updated to the UI.
+    private void subscribeObservers() {
+       viewModel.observeUser().observe(this, new Observer<User>() {
+           @Override
+           public void onChanged(User user) {
+
+               // Ensure User isn't null & is successfully authenticated.
+               if (user != null) {
+                   Log.d(TAG, "onChanged: " + user.getEmail());
+               }
+           }
+       });
     }
 
     // Method to set a logo
@@ -48,5 +78,28 @@ public class AuthActivity extends DaggerAppCompatActivity {
         requestManager
                 .load(logo)
                 .into((ImageView) findViewById(R.id.login_logo));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.login_button: {
+                attemptLogin();
+                break;
+            }
+        }
+    }
+
+    private void attemptLogin() {
+
+        // Check for null user id
+        if (TextUtils.isEmpty(userId.getText().toString())) {
+            return;
+        }
+
+        // Attempt login when user id is not null
+        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
+
     }
 }
