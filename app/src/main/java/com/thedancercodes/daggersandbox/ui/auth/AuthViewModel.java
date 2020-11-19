0cfc411a85283fbf5f1761be9d74f8b3fe9196ae
Taracha <rojtaracha@gmail.com>
2020-11-19 13:26:04 +0300

    Authentication Network Resource Class: * ui/auth/AuthResource
    * AuthViewModel - Leverage the AuthResource in the MediatorLiveData.
      * It wraps around the User object.
      * Update implementations in the LiveDataReactiveStreams and addSource() and observeUser
    
    * AuthActivity
      * Add a ProgressBar
        * Create showProgressBar() method.
      * Update subscribeObservers() to use the AuthResource
        * Set the 4 statuses in the onChanged method.
        * NOTE: When we set a value in the ViewModel, the OnChanged method on AuthActivity will trigger if the status changes and we can update the UI.

--- app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/thedancercodes/daggersandbox/ui/auth/AuthViewModel.java
@@ -13,6 +13,8 @@ import com.thedancercodes.daggersandbox.network.auth.AuthApi;
 
 import javax.inject.Inject;
 
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
@@ -21,7 +23,7 @@ public class AuthViewModel extends ViewModel {
 
     private final AuthApi authApi;
 
-    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
 
     @Inject
     public AuthViewModel(AuthApi authApi) {
@@ -32,14 +34,46 @@ public class AuthViewModel extends ViewModel {
     // Initiate call to the API. The source is the API call from the Flowable object.
     // Here we are converting the Flowable to the LiveData object.
     public void authenticateWithId(int userId) {
-        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+
+        // Set the Loading Status
+        authUser.setValue(AuthResource.loading((User)null));
+
+        final LiveData<AuthResource<User>> source = LiveDataReactiveStreams.fromPublisher(
                 authApi.getUser(userId)
+
+                        // Called if error occurs (instead of calling onError)
+                        .onErrorReturn(new Function<Throwable, User>() {
+                            @NonNull
+                            @Override
+                            public User apply(@NonNull Throwable throwable) throws Exception {
+                                User errorUser = new User();
+                                errorUser.setId(-1);
+                                return errorUser;
+                            }
+                        })
+
+                        // This operator receives the errorUser object if there was an error OR
+                        // receive the User object from the API if there was no error.
+                        .map(new Function<User, AuthResource<User>>() {
+                            @NonNull
+                            @Override
+                            public AuthResource<User> apply(@NonNull User user) throws Exception {
+
+                                // Return message if there's an error
+                                if (user.getId() == -1){
+                                    return AuthResource.error("Could not authenticate", (User)null);
+                                }
+
+                                // Authenticate if there no error
+                                return AuthResource.authenticated(user);
+                            }
+                        })
                         .subscribeOn(Schedulers.io()));
 
         // Setting source (above) to authUser (below) using MediatorLiveData
-        authUser.addSource(source, new Observer<User>() {
+        authUser.addSource(source, new Observer<AuthResource<User>>() {
             @Override
-            public void onChanged(User user) {
+            public void onChanged(AuthResource<User> user) {
                 // Update any observes using setValue()
                 authUser.setValue(user);
 
@@ -50,7 +84,7 @@ public class AuthViewModel extends ViewModel {
     }
 
     // LiveData object of required data type (User) that we are returning to the UI
-    public LiveData<User> observeUser() {
+    public LiveData<AuthResource<User>> observeUser() {
         return authUser;
     }
 
