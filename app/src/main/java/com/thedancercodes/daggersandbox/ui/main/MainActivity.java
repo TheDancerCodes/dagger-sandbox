af17845147c3a9f5a478a476c816055424f10bfe
Taracha <rojtaracha@gmail.com>
2020-11-22 18:54:24 +0300

    Toolbar Menu and Logout * res/menu/main_menu
    * Add logout string resource.
    
    * Add main_menu to MainActivity.

--- app/src/main/java/com/thedancercodes/daggersandbox/ui/main/MainActivity.java
+++ app/src/main/java/com/thedancercodes/daggersandbox/ui/main/MainActivity.java
@@ -1,8 +1,12 @@
 package com.thedancercodes.daggersandbox.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.thedancercodes.daggersandbox.BaseActivity;
@@ -18,5 +22,27 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // Switch statement for catching the different clicks to the menu items.
+        switch (item.getItemId()) {
+
+            case R.id.logout: {
+                sessionManager.logOut();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
