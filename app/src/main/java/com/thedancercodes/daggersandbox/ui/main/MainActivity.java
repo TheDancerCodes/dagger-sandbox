215a81ab6ae11238f28514ad00456e5e10219af6
Taracha <rojtaracha@gmail.com>
2020-11-23 21:45:40 +0300

    Navigation Drawer AndroidX * Create a menu for the navigation.   * menu/drawer_view.xml
    * Add string resources.
      * activity_main.xml
        * Refactor layout to include a navigation drawer layout, navigation view, etc.
    
    * MainActivity
      * Add setup code for NavigationDrawer and menu item click functionality.

--- app/src/main/java/com/thedancercodes/daggersandbox/ui/main/MainActivity.java
+++ app/src/main/java/com/thedancercodes/daggersandbox/ui/main/MainActivity.java
@@ -4,24 +4,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.thedancercodes.daggersandbox.BaseActivity;
 import com.thedancercodes.daggersandbox.R;
 import com.thedancercodes.daggersandbox.ui.main.posts.PostsFragment;
-import com.thedancercodes.daggersandbox.ui.main.profile.ProfileFragment;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         testFragment();
     }
@@ -52,5 +59,27 @@ public class MainActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        // Switch statement to handle when menu items are clicked
+        switch (item.getItemId()) {
+
+            case R.id.nav_profile: {
+
+                break;
+            }
+
+            case R.id.nav_posts: {
+
+                break;
+            }
+        }
+
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
