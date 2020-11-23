f788e616be8481caa336f3a5556b6ae7654df5f8
Roger Taracha <rojtaracha@gmail.com>
2020-11-23 10:20:18 +0300

    Injecting Fragments with DaggerFragment (#16)
    * Create layout for the new fragment class.
      * layout/fragment_profile
    
    * ui/main/profile/ProfileFragment
    
    * We want the ProfileFragment to exist in the MainActivity SubComponent.
      * di/main/MainFragmentBuildersModule
        * This class will contain all the injectors for the Fragments inside MainComponent.
        * We will add this class as a module for MainActivity in ActivityBuildersModule.
        * This means this fragment will only exist within the scope of the MainActivity SubComponent.
    
    * Run app to ensure fragment is coming into view.

---
+++ app/src/main/java/com/thedancercodes/daggersandbox/di/main/MainFragmentBuildersModule.java
@@ -0,0 +1,17 @@
+package com.thedancercodes.daggersandbox.di.main;
+
+import com.thedancercodes.daggersandbox.ui.main.profile.ProfileFragment;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+/**
+ * This class will contain all the injectors for the Fragments inside MainComponent.
+ */
+@Module
+public abstract class MainFragmentBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract ProfileFragment contributeProfileFragment();
+
+}
